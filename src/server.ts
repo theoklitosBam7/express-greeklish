fe1fa23df255bdf9684afe14eb0eeb887928eb14
theoklitosBam7 <th.bampouris@gmail.com>
2021-06-29 13:40:43 +0300

    feat: initialize express server

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+function start() {
+  if (!process.env.NODE_ENV || !process.env.PORT) {
+    console.error(
+      'ENV variables are missing.',
+      'Verify that you have set them directly or in a .env file.'
+    );
+    process.exit(1);
+  } else {
+    console.log('Using ENV variables');
+  }
+
+  const app = express();
+  const port = process.env.PORT || 7070;
+  const www = process.env.WWW || './';
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.use(express.static(www));
+  console.log(`serving ${www}`);
+
+  app.get('*', (req, res) => {
+    res.sendFile('index.html', { root: www });
+  });
+  app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+}
+
+export { start };
+
